3fde2f58c9541b516f0b7926cfe144e87132c0c9
Irina Nicolai <irina.nic.yu@gmail.com>
2022-04-05 10:04:41 +0200

    Add albums request

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,27 @@
-import { LOAD_ALBUMS } from './actionTypes';
+import { LOAD_ALBUMS, REQUEST, SUCCESS, FAILURE } from './actionTypes';
+import { URL } from '../utils/variables';
 
-export const loadAlbums = (searchString) => ({
-    type: LOAD_ALBUMS,
-    data: searchString,
-});
+export const loadAlbums = (searchString) => async (dispatch) => {
+    dispatch({
+        type: `${LOAD_ALBUMS}__${REQUEST}`,
+        data: [],
+    });
+
+    try {
+        const data = await fetch(URL(searchString)).then(res => res.json());
+        dispatch({
+            type: `${LOAD_ALBUMS}__${SUCCESS}`,
+            data,
+        });
+
+    } catch (error) {
+        dispatch({
+            type: `${LOAD_ALBUMS}__${FAILURE}`,
+            data: [],
+            error,
+        });
+    }
+
+};
+
+
--- src/store/reducers/albumsReducer.js
+++ src/store/reducers/albumsReducer.js
@@ -3,8 +3,9 @@ import { LOAD_ALBUMS, SUCCESS } from '../actionTypes';
 const albumsReducer = (state = [], action) => {
 
     switch (action.type) {
-        case LOAD_ALBUMS:
-            return [...state, action.data];
+        case `${LOAD_ALBUMS}__${SUCCESS}`:
+            const { resultCount, results } = action.data;
+            return [...state, ...results];
 
         default:
             return state;
